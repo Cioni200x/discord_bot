3d407b03b638f171381c91c4eea7fa7a9cdcc5a2
Cioni200x <96991242+Cioni200x@users.noreply.github.com>
2022-02-21 19:57:40 +0300

    Update discord_Bot.py

--- discord_Bot.py
+++ discord_Bot.py
@@ -1,16 +1,18 @@
+from http import client
 import discord
 import config
 from discord.ext import commands 
 
-client = discord.Client()
-bot = commands.Bot(command_prefix='$')
+client = commands.Bot(command_prefix='>')
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('Bot is ready')
 
-@bot.command()
-async def test(ctx, arg):
-    await ctx.send(arg)
+@client.command()
+async def hello(ctx):
+    author = ctx.message.author 
+
+    await ctx.send(f'Hello, {author.mention}!')
 
 client.run(config.TOKEN)
